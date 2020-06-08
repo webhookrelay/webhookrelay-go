72183c36d1cffeeb02257392578afa270722f3fe
Karolis Rusenas <karolis.rusenas@gmail.com>
2020-06-08 23:38:49 +0100

    bucket options

--- pkg/client/buckets.go
+++ pkg/client/buckets.go
@@ -3,6 +3,7 @@ package client
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 
@@ -67,9 +68,6 @@ type BucketAuth struct {
 type BucketCreateOptions struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
-
-	// Context can be set with a timeout or can be used to cancel a request.
-	Context context.Context `json:"-"`
 }
 
 // BucketUpdateOptions - bucket update opts
@@ -99,4 +97,63 @@ func (api *API) ListBuckets(options *BucketListOptions) ([]*Bucket, error) {
 	}
 
 	return buckets, nil
+}
+
+// GetBucket gets specific bucket
+func (api *API) GetBucket(ref string) (*Bucket, error) {
+
+	ref, err := api.ensureBucketID(ref)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := api.makeRequest("GET", "/buckets/"+ref, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var result Bucket
+	if err := json.Unmarshal(resp, &result); err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+// CreateBucket creates a Bucket and returns the newly object.
+func (api *API) CreateBucket(opts *BucketCreateOptions) (*Bucket, error) {
+	resp, err := api.makeRequest("POST", "/buckets", opts)
+	if err != nil {
+		return nil, err
+	}
+
+	var bucket Bucket
+	if err := json.Unmarshal(resp, &bucket); err != nil {
+		return nil, err
+	}
+	return &bucket, nil
+}
+
+// ensureBucketID - takes name/id and always returns ID (when it not fails)
+func (api *API) ensureBucketID(ref string) (string, error) {
+	if !IsUUID(ref) {
+		id, err := api.bucketIDFromName(ref)
+		if err != nil {
+			return "", err
+		}
+		return id, nil
+	}
+	return ref, nil
+}
+
+func (api *API) bucketIDFromName(name string) (id string, err error) {
+	buckets, err := api.ListBuckets(&BucketListOptions{})
+	if err != nil {
+		return
+	}
+	for _, b := range buckets {
+		if b.Name == name {
+			return b.ID, nil
+		}
+	}
+	return "", fmt.Errorf("no such bucket '%s'", name)
 }
