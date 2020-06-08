13d5fd608ca330352382e66e9bc52e6a2df6103e
Karolis Rusenas <karolis.rusenas@gmail.com>
2020-06-09 00:26:15 +0100

    inputs list

--- pkg/client/inputs.go
+++ pkg/client/inputs.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"encoding/json"
+	"fmt"
 	"time"
 )
 
@@ -55,4 +56,45 @@ func (i *Input) UnmarshalJSON(data []byte) error {
 	i.CreatedAt = time.Unix(aux.CreatedAt, 0)
 	i.UpdatedAt = time.Unix(aux.UpdatedAt, 0)
 	return nil
+}
+
+type InputListOptions struct {
+	Bucket string // Bucket reference - ID or name
+}
+
+// ListInputs returns a list of inputs belonging to the bucket. If bucket reference not supplied,
+// all account inputs will be returned
+func (api *API) ListInputs(options *InputListOptions) ([]*Input, error) {
+
+	if options.Bucket == "" {
+		return api.allInputList(&BucketListOptions{})
+	}
+
+	bucket, err := api.GetBucket(options.Bucket)
+	if err != nil {
+		return nil, err
+	}
+
+	var inputs []*Input
+	for idx := range bucket.Inputs {
+		inputs = append(inputs, &bucket.Inputs[idx])
+	}
+
+	return inputs, nil
+}
+
+func (api *API) allInputList(opts *BucketListOptions) ([]*Input, error) {
+	buckets, err := api.ListBuckets(opts)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get inputs, error: %w", err)
+	}
+
+	var inputs []*Input
+	for idx := range buckets {
+		for bIdx := range buckets[idx].Inputs {
+			inputs = append(inputs, &buckets[idx].Inputs[bIdx])
+		}
+	}
+
+	return inputs, nil
 }
