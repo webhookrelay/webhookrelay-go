e064d71b64e8399f985fdc4779d9638fa00d3359
Karolis Rusenas <karolis.rusenas@gmail.com>
2020-06-09 00:05:33 +0100

    errors

--- pkg/client/errors.go
+++ pkg/client/errors.go
@@ -2,7 +2,13 @@ package client
 
 import "errors"
 
-// Error messages
+// Errors
 var (
 	ErrEmptyCredentials = errors.New("invalid credentials: key & secret must not be empty")
+)
+
+// Error messages
+var (
+	errMakeRequestError = "error from makeRequest"
+	errUnmarshalError   = "error while unmarshalling the JSON response"
 )
