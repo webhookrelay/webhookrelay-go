dd57350cf29bb48ff9a3fd915167533cd62aca1d
Karolis Rusenas <karolis.rusenas@gmail.com>
2020-06-09 10:08:37 +0100

    version

---
+++ pkg/client/version.go
@@ -0,0 +1,35 @@
+package client
+
+import (
+	"context"
+	"encoding/json"
+)
+
+// VersionInfo describes version and runtime info.
+type VersionInfo struct {
+	Name          string `json:"name"`
+	BuildDate     string `json:"buildDate"`
+	Revision      string `json:"revision"`
+	Version       string `json:"version"`
+	APIVersion    string `json:"apiVersion"`
+	GoVersion     string `json:"goVersion"`
+	OS            string `json:"os"`
+	Arch          string `json:"arch"`
+	KernelVersion string `json:"kernelVersion"`
+	Experimental  bool   `json:"experimental"`
+}
+
+// ServerVersion returns the server's version and runtime info.
+func (api *API) ServerVersion(ctx context.Context) (*VersionInfo, error) {
+
+	resp, err := api.makeRequest("GET", "/version", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var version VersionInfo
+	if err := json.Unmarshal(resp, &version); err != nil {
+		return nil, err
+	}
+	return &version, nil
+}
